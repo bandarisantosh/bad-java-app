9b59e1898c0aedd34fd1a097c073d59279f55af5
Santosh <santosh@semgrep.com>
2025-10-21 23:23:11 +0530

    Update createScorecards.sh

--- createScorecards.sh
+++ createScorecards.sh
@@ -2,3 +2,6 @@ source "scripts/verifyBenchmarkPluginAvailable.sh"
 #mvn -Djava.awt.headless=true org.owasp:benchmarkutils-maven-plugin:create-scorecard -DconfigFile=config/score_v1.3config.yaml
 mvn -Djava.awt.headless=true org.owasp:benchmarkutils-maven-plugin:create-scorecard
 
+source "scripts/verifyBenchmarkPluginAvailable.sh"
+#mvn -Djava.awt.headless=true org.owasp:benchmarkutils-maven-plugin:create-scorecard -DconfigFile=config/score_v1.3config.yaml
+mvn -Djava.awt.headless=true org.owasp:benchmarkutils-maven-plugin:create-scorecard
